a32ab3d76f5f64653ce30452e3339900504c05fd
vidhya <vidhya@DESKTOP-5P9E2KH>
2023-01-25 18:15:25 +0530

    this is my cls2_vijay class

--- src/MS/Aveera.java
+++ src/MS/Aveera.java
@@ -4,6 +4,7 @@ public class Aveera {
 	
 	public static void main(String[] args) {
 		System.out.println("Hi, how are you");
+		System.out.println("hi  i am vijay");
 	}
 
 }
---
+++ src/MS/Cls2_vijay.java
@@ -0,0 +1,8 @@
+package MS;
+
+public class Cls2_vijay {
+
+	public static void main(String[] args) {
+		System.out.println("i am vijay");
+	}
+}
